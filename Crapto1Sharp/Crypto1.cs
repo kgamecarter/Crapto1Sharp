752d901f1cac35ea8ef917977f123406f3ffc0bf
kgame <kgamecarter@gmail.com>
2018-10-16 01:06:37 +0800

    complete crypto1 feature

--- Crapto1Sharp/Crypto1.cs
+++ Crapto1Sharp/Crypto1.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using Crapto1Sharp.Extensions;
 
 namespace Crapto1Sharp
 {
     public class Crypto1
     {
+        public const uint LF_POLY_ODD = 0x29CE5C;
+        public const uint LF_POLY_EVEN = 0x870804;
+
         protected Crypto1State _state;
 
         public Crypto1State State
         {
             get
-            {
-                return _state;
-            }
+            { return _state; }
+            set
+            { _state = value; }
         }
 
         public ulong Lfsr
@@ -21,8 +24,8 @@ namespace Crapto1Sharp
                 ulong lfsr = 0L;
                 for (int i = 23; i >= 0; --i)
                 {
-                    lfsr = lfsr << 1 | ((_state.Odd >> i ^ 3) & 1);
-                    lfsr = lfsr << 1 | ((_state.Even >> i ^ 3) & 1);
+                    lfsr = lfsr << 1 | _state.Odd.Bit(i ^ 3);
+                    lfsr = lfsr << 1 | _state.Even.Bit(i ^ 3);
                 }
                 return lfsr;
             }
@@ -35,9 +38,97 @@ namespace Crapto1Sharp
         {
             for (int i = 47; i > 0; i -= 2)
             {
-                _state.Odd = State.Odd << 1 | (uint)((key >> (i - 1) ^ 7) & 1);
-                _state.Even = _state.Even << 1 | (uint)((key >> (i ^ 7)) & 1);
+                _state.Odd = State.Odd << 1 | key.Bit((i - 1) ^ 7);
+                _state.Even = _state.Even << 1 | key.Bit(i ^ 7);
             }
+        }
+
+        public byte Crypto1Bit(byte _in = 0, bool isEncrypted = false)
+        {
+            uint feedin;
+            byte ret = Filter(_state.Odd);
+
+            feedin = ret & (isEncrypted ? 1u : 0u);
+            feedin ^= _in > 0 ? 1u : 0u;
+            feedin ^= LF_POLY_ODD & State.Odd;
+            feedin ^= LF_POLY_EVEN & _state.Even;
+            _state.Even = _state.Even << 1 | EvenParity32(feedin);
+
+            uint x = State.Odd;
+            _state.Odd = _state.Even;
+            _state.Even = x;
+
+            return ret;
+        }
+
+        public byte Crypto1Byte(byte _in = 0, bool isEncrypted = false)
+        {
+            byte ret = 0;
+
+            for (int i = 0; i < 8; ++i)
+                ret |= (byte)(Crypto1Bit(_in.Bit(i), isEncrypted) << i);
+
+            return ret;
+        }
+
+        public byte Crypto1Word(uint _in = 0, bool isEncrypted = false)
+        {
+            byte ret = 0;
+
+            for (int i = 0; i < 32; ++i)
+                ret |= (byte)(Crypto1Bit(_in.BeBit(i), isEncrypted) << i);
+
+            return ret;
+        }
+
+
+        public static byte Filter(uint x)
+        {
+            uint f;
+            f = 0xf22c0u >> (int)(x & 0xf) & 16u;
+            f |= 0x6c9c0u >> (int)(x >> 4 & 0xf) & 8u;
+            f |= 0x3c8b0u >> (int)(x >> 8 & 0xf) & 4u;
+            f |= 0x1e458u >> (int)(x >> 12 & 0xf) & 2u;
+            f |= 0x0d938u >> (int)(x >> 16 & 0xf) & 1u;
+            return (byte)(0xEC57E80A >> (int)f & 1);
+        }
+
+        static readonly byte[] OddByteParity = new byte[256] {
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+        };
+
+        public static byte OddParity8(byte x) => OddByteParity[x];
+
+        public static byte EvenParity8(byte x) => (byte)(OddByteParity[x] ^ 1);
+
+        public static byte EvenParity32(uint x)
+        {
+            x ^= x >> 16;
+            x ^= x >> 8;
+            return EvenParity8((byte)x);
+        }
+
+        public static uint PrngSuccessor(uint x, int n)
+        {
+            x = x.SwapEndian();
+            while (n-- > 0)
+                x = x >> 1 | (x >> 16 ^ x >> 18 ^ x >> 19 ^ x >> 21) << 31;
+            return x.SwapEndian();
         }
     }
 }
---
+++ Crapto1Sharp/Extensions/UIntExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crapto1Sharp.Extensions
+{
+    public static class UIntExtension
+    {
+        public static byte Bit(this byte v, int n)
+        {
+            return (byte)(v >> n & 1);
+        }
+
+        public static byte Bit(this uint v, int n)
+        {
+            return (byte)(v >> n & 1);
+        }
+
+        public static byte Bit(this ulong v, int n)
+        {
+            return (byte)(v >> n & 1);
+        }
+
+        public static byte BeBit(this uint v, int n)
+        {
+            return v.Bit(n ^ 24);
+        }
+
+        public static uint SwapEndian(this uint x)
+        {
+            x = (x >> 8 & 0xff00ff) | (x & 0xff00ff) << 8;
+            x = x >> 16 | x << 16;
+            return x;
+        }
+    }
+}
