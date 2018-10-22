bd891da025436bd5d98e7182042cb9fde407465e
kgame <kgamecarter@gmail.com>
2018-10-23 01:03:43 +0800

    test PeekCrypto1Bit, OddParity8, EvenParity8

--- Crapto1Sharp.Test/Crypto1Test.cs
+++ Crapto1Sharp.Test/Crypto1Test.cs
@@ -40,5 +40,50 @@ namespace Crapto1Sharp.Test
             var crypto1 = new Crypto1(key);
             Assert.AreEqual(crypto1.Lfsr, key);
         }
+
+
+        [TestMethod]
+        [DataRow(0xd73A52b491AAuL)]
+        [DataRow(0x9D29AE25242AuL)]
+        [DataRow(0x1FA3E73CAC0AuL)]
+        [DataRow(0xD1C9DB532E82uL)]
+        [DataRow(0x239186C46E88uL)]
+        public void PeekCrypto1Bit(ulong key)
+        {
+            var crypto1 = new Crypto1(key);
+            Assert.AreEqual(crypto1.PeekCrypto1Bit(), Crypto1.Filter(crypto1.State.Odd));
+        }
+
+        [TestMethod]
+        public void OddParity8()
+        {
+            for (int i = 0; i <= 0xff; i++)
+            {
+                var count = 1;
+                var v = i;
+                while (v > 0)
+                {
+                    count += v & 1;
+                    v >>= 1;
+                }
+                Assert.AreEqual(Crypto1.OddParity8((byte)i), (byte)(count % 2));
+            }
+        }
+
+        [TestMethod]
+        public void EvenParity8()
+        {
+            for (int i = 0; i <= 0xff; i++)
+            {
+                var count = 0;
+                var v = i;
+                while (v > 0)
+                {
+                    count += v & 1;
+                    v >>= 1;
+                }
+                Assert.AreEqual(Crypto1.EvenParity8((byte)i), (byte)(count % 2));
+            }
+        }
     }
 }
