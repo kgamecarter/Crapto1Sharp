acc6ae8e774bdc8033471a8ae1aa203cdeb01147
kgame <kgamecarter@gmail.com>
2018-10-23 20:18:48 +0800

    remove QuickSort implement

--- Crapto1Sharp/Extensions/SpanExtension.cs
+++ Crapto1Sharp/Extensions/SpanExtension.cs
@@ -9,6 +9,7 @@ namespace Crapto1Sharp.Extensions
 {
     internal static class SpanExtension
     {
+#if !NET45
         // wait Span.Sort() implement https://github.com/dotnet/corefx/issues/15329
         /// <summary>
         /// Quick Sort
@@ -17,44 +18,11 @@ namespace Crapto1Sharp.Extensions
         /// <param name="span"></param>
         public static void Sort<T>(this Span<T> span)
         {
-#if !NET45
             var a = span.ToArray();
             Array.Sort(a);
             a.CopyTo(span);
-            return;
-#endif
-            if (span.Length <= 1)
-                return;
-            var it = 0;
-            var rit = span.Length - 1;
-            var comparer = Comparer<T>.Default;
-
-            while (it < rit)
-            {
-                if (comparer.Compare(span[it], span[0]) <= 0)
-                    ++it;
-                else if (comparer.Compare(span[rit], span[0]) > 0)
-                    --rit;
-                else
-                {
-                    var t = span[it];
-                    span[it] = span[rit];
-                    span[rit] = t;
-                }
-            }
-
-            if (comparer.Compare(span[rit], span[0]) >= 0)
-                --rit;
-            if (rit != 0)
-            {
-                var t = span[rit];
-                span[rit] = span[0];
-                span[0] = t;
-            }
-            
-            span.Slice(0, rit).Sort();
-            span.Slice(rit + 1).Sort();
         }
+#endif
 
         public static int BinarySearch(this Span<uint> span)
         {
